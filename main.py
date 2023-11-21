8cda33eb9dbae174ae4e2c7fc9b1e01ee965e95f
sarthaklambaa <samlamba29@gmail.com>
2023-11-21 17:48:45 +0530

    Fixed Database name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qr_ticket.db'
 db = SQLAlchemy(app)
 
 class FormData(db.Model):
